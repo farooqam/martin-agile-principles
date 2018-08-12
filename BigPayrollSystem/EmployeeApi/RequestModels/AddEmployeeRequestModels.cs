7d1ae34e433a39322469df54c322b3ba6117a80d
Farooq Mahmud <farooq_am@hotmail.com>
2018-08-11 21:03:06 -0700

    Employee API Controller  - made AddEmployeeRequest model abstract; sealed model classes derived from it.

--- BigPayrollSystem/EmployeeApi/RequestModels/AddEmployeeRequestModels.cs
+++ BigPayrollSystem/EmployeeApi/RequestModels/AddEmployeeRequestModels.cs
@@ -1,23 +1,23 @@
 namespace BigCorp.EmployeeApi.RequestModels
 {
-    public class AddEmployeeRequestModel
+    public abstract class AddEmployeeRequestModel
     {
         public string Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
     }
 
-    public class AddSalariedEmployeeRequestModel : AddEmployeeRequestModel
+    public sealed class AddSalariedEmployeeRequestModel : AddEmployeeRequestModel
     {
         public decimal Salary { get; set; }
     }
 
-    public class AddHourlyEmployeeRequestModel : AddEmployeeRequestModel
+    public sealed class AddHourlyEmployeeRequestModel : AddEmployeeRequestModel
     {
         public decimal Rate { get; set; }
     }
 
-    public class AddCommissionedEmployeeRequestModel : AddEmployeeRequestModel
+    public sealed class AddCommissionedEmployeeRequestModel : AddEmployeeRequestModel
     {
         public decimal Salary { get; set; }
         public decimal Rate { get; set; }
